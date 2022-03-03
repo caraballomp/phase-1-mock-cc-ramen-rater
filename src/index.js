d97b5de71e3239468634bb74fa61d90f71628226
Michelle Caraballo <caraballomp@yahoo.com>
2022-03-03 13:45:56 -0800

    submission for deadline

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 // write your code here
+const ramenAPI = "http://localhost:3000/ramens";
+const ramenMenu = document.getElementById("ramen-menu");
+const ramenDetail =document.getElementByName("ramen-detail")
+const commentdisplay= document.getElementByName("comment-display")
+
+
+fetch(ramenAPI)
+.then((res) => res.json())
+.then(displayRamenImages)
+.catch(console.err);
+
+document.getElementById('new-ramen').addEventListener('submit', addNewRamen);
